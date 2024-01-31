205161ac86dc60484745010f865fd2406090c028
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2024-01-31 17:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(value) {
+    this._age = value;
+  }
+}
 
-// Do not change the code below this line
+class Student extends Person {
+  // Constructor for Student class
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method specific to Student class
+  study() {
+    console.log(${this.name} is studying);
+  }
+}
+
+class Teacher extends Person {
+  // Constructor for Teacher class
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method specific to Teacher class
+  teach() {
+    console.log(${this.name} is teaching);
+  }
+}
+
+// Assigning classes to the window object
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
